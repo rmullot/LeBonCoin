30fffd7f5464396c415a5e6aa450f4ffe0616ccd
Romain Mullot <romainmullot@free.fr>
2020-02-07 08:49:44 +0100

    Updated filter feature

--- test_Romain_MULLOT/Filter/FilterViewController.swift
+++ test_Romain_MULLOT/Filter/FilterViewController.swift
@@ -13,10 +13,29 @@ final class FilterViewController: UIViewController {
     
     private let viewModel: FilterViewModelProtocol
     
+    private let categoryHeightRow: CGFloat = 80
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView.autolayout()
+        tableView.backgroundColor = .white
+        tableView.accessibilityIdentifier = "FilterViewController_tableView"
+        tableView.allowsMultipleSelection = true
+        tableView.isOpaque = true
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = categoryHeightRow
+        tableView.registerReusableCell(CategoryCell.self)
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         setup()
+        // Do any additional setup after loading the view.
+        viewModel.refreshCategoriesList { [weak self] in
+            guard let strongSelf = self else { return }
+            strongSelf.tableView.reloadData()
+        }
     }
     
     init(viewModel: FilterViewModelProtocol) {
@@ -33,6 +52,10 @@ final class FilterViewController: UIViewController {
 private extension FilterViewController {
     
     func setup() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: .didTapCancelButton)
+        navigationItem.leftBarButtonItem?.tintColor = .orange
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: .didTapValidateButton)
+        navigationItem.rightBarButtonItem?.tintColor = .orange
         view.isOpaque = true
         view.backgroundColor = .white
         setupInterface()
@@ -40,10 +63,70 @@ private extension FilterViewController {
     }
     
     func setupInterface() {
-        
+        view.addSubview(tableView)
     }
     
     func setupConstraints() {
-        
+        tableView.anchorToBounds(view: view)
     }
+}
+
+extension FilterViewController {
+    
+    @objc
+    func didTapCancelButton(_ sender: UIButton) {
+        viewModel.didTapCancel()
+    }
+    
+    @objc
+    func didTapValidateButton(_ sender: UIButton) {
+        viewModel.didTapValidate()
+    }
+}
+
+extension FilterViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return categoryHeightRow
+    }
+
+}
+
+extension FilterViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.categoriesCount
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(CategoryCell.self, indexPath: indexPath)
+        cell.config(viewModel: viewModel, index: indexPath.row)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let categoryCell = cell as? CategoryCell else { return }
+        categoryCell.config(viewModel: viewModel, index: indexPath.row)
+//        if cell.isSelected {
+//            cell.accessoryType = .checkmark
+//            cell.isSelected = true
+//            cell.layoutIfNeeded()
+//        } else {
+//            cell.accessoryType = .none
+//        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        viewModel.didSelectCategory(index: indexPath.row)
+    }
+    
+}
+
+private extension Selector {
+    static let didTapCancelButton = #selector(FilterViewController.didTapCancelButton(_:))
+    static let didTapValidateButton = #selector(FilterViewController.didTapValidateButton(_:))
 }
--- test_Romain_MULLOT/Filter/FilterViewModel.swift
+++ test_Romain_MULLOT/Filter/FilterViewModel.swift
@@ -7,9 +7,18 @@
 //
 
 import Foundation
+import LBCBridge
 
 protocol FilterViewModelProtocol: AnyObject {
     var delegate: FilterViewModelDelegate? { get set }
+    func didTapCancel()
+    func didTapValidate()
+    var categoriesCount: Int { get }
+    func getName(index: Int) -> String
+    func isSelected(index: Int) -> Bool
+    func didSelectCategory(index: Int)
+    func refreshCategoriesList(completionHandler: @escaping ()->())
+    init(categoryService: CategoryService)
 }
 
 protocol FilterViewModelDelegate: AnyObject {
@@ -18,5 +27,67 @@ protocol FilterViewModelDelegate: AnyObject {
 }
 
 final class FilterViewModel: FilterViewModelProtocol {
+    
     weak var delegate: FilterViewModelDelegate?
+    
+    private var categories: [LBCBridge.Category]? = nil
+    private var selectedCategories: [Int] = []
+    private let categoryService: CategoryService
+    
+    var categoriesCount: Int {
+        return categories?.count ?? 0
+    }
+    
+    init(categoryService: CategoryService) {
+        self.categoryService = categoryService
+    }
+    
+    func didTapCancel() {
+        delegate?.didTapCancel(viewModel: self)
+    }
+    
+    func didTapValidate() {
+        categoryService.selectedCategories = selectedCategories
+        delegate?.didTapValidate(viewModel: self)
+    }
+    
+    func didSelectCategory(index: Int) {
+        guard let category = getCategory(index: index) else { return }
+        
+        if let index = selectedCategories.firstIndex(of: category.idCategory) {
+            selectedCategories.remove(at: index)
+        } else {
+            selectedCategories.append(category.idCategory)
+        }
+    }
+    
+    func getName(index: Int) -> String {
+        return getCategory(index: index)?.name ?? ""
+    }
+    
+    func isSelected(index: Int) -> Bool {
+        guard let category = getCategory(index: index) else { return false }
+        return selectedCategories.contains(category.idCategory)
+    }
+    
+    func refreshCategoriesList(completionHandler: @escaping ()->()) {
+        categoryService.getCategories { [weak self] (result) in
+            guard let strongSelf = self else { return }
+            switch result {
+            case .success(let categories):
+                strongSelf.categories = categories ?? []
+                strongSelf.selectedCategories = strongSelf.categoryService.selectedCategories
+                completionHandler()
+            default: completionHandler()
+            }
+        }
+    }
+}
+
+private extension FilterViewModel {
+    
+    func getCategory(index: Int) -> LBCBridge.Category? {
+        guard categories?.isValidIndex(index) ?? false, let category = categories?[index] else { return nil }
+        return category
+    }
 }
