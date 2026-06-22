f2490b34ce00768f00dcbbdea31c0c1b87cd3cff
Romain MULLOT <romainmullot@free.fr>
2026-06-22 15:36:00 +0200

    feature: one spm setup per framework done

---
+++ LBCUIKit/Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "LBCUIKit",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "LBCUIKit", targets: ["LBCUIKit"]),
+    ],
+    dependencies: [
+        .package(path: "../LBCCore"),
+        .package(path: "../LBCNetwork"),
+    ],
+    targets: [
+        .target(
+            name: "LBCUIKit",
+            dependencies: [
+                .product(name: "LBCCore", package: "LBCCore"),
+                .product(name: "LBCNetwork", package: "LBCNetwork"),
+            ],
+            path: "LBCUIKit",
+            exclude: ["Info.plist", "LBCUIKit.h"]
+        ),
+    ]
+)
